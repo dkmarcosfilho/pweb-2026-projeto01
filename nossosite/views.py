feb21d7fcff5e2a815ab9eeffd6cc0e9f25008d5
Bruno Rafael <br781642@gmail.com>
2026-06-03 09:27:46 -0300

    Comitando

--- nossosite/views.py
+++ nossosite/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def elenco(request):
+    return render(request, "elenco.html")
+
+def sobre(request):
+    return render(request, "sobre.html")
